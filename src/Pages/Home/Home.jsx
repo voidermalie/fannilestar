165b5e30b69b161b5756418ef92e985c29d93029
voidermalie <lestarfanny@gmail.com>
2023-05-10 19:09:55 +0200

    design

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -11,19 +11,13 @@ const Home = () => {
             Ma passion est l&apos;interconnectivité de notre monde, dans toutes
             ses dimensions, y compris le Web 🌐🕸️ <br /> L&apos;écologie me
             tient particulièrement à cœur, je m&apos;efforce à concevoir des
-            sites dans une approche éco-responsible
-            <br /> et espère à travailler sur des projets qui protègent la vie.
+            sites dans une approche éco-responsible et espère à travailler sur des projets qui protègent la vie ou ajoutent un petit + à notre monde.
             <br />
             Traductrice professionnelle avant ma reconversion au développement
-            web (diplôme RNCP OpenClassrooms, auto-apprentissage Codecademy),
-            <br />
-            c&apos;est toujours le langage humain que je traduis. Trilingue, on
+            web (diplôme RNCP OpenClassrooms, auto-didacte Codecademy),
+            c&apos;est toujours le langage humain que je traduis. Je suis trilingue, on
             peut communiquer en français, anglais ou en hongrois :) <br />
             Et aussi en <span>HTML CSS JavaScript React</span>
-          </p>
-          <p>
-            Je suis à Marseille et cherche des projets et équipes à joindre ici
-            dans le Sud ou à distance dans le monde.
           </p>
         </article>
         <article id="bio" className="main-card middle-card">
