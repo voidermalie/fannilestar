4812136d6d6f5f6b55fa1741627a22521498b683
voidermalie <lestarfanny@gmail.com>
2023-08-02 19:28:50 +0200

    projectpopup whats up

--- src/Components/ProjectPopup/ProjectPopup.jsx
+++ src/Components/ProjectPopup/ProjectPopup.jsx
@@ -1,9 +1,9 @@
 import './ProjectPopup.css';
 import { ProjectContext } from '../../contexts/ProjectContext';
 import { LanguageContext } from '../../contexts/LanguageContext';
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 
-import MimicOctopus from '../../assets/mimic_octopus.gif'
+import MimicOctopus from '../../assets/mimic_octopus.gif';
 
 const ProjectPopup = ({ id, setIsOpen }) => {
   const { language } = useContext(LanguageContext);
@@ -20,42 +20,52 @@ const ProjectPopup = ({ id, setIsOpen }) => {
 
   return (
     <div id="popup-wrapper">
-    <section className="popup-background">
-      <button
-        className="close-button"
-        type="button"
-        aria-label="fermer"
-
-        onClick={() => { setIsOpen(false)}}
-      >
-        <span aria-hidden="true">&times;</span>
-      </button>
-      <div className="project-container">
-        <header className='popup-header'>
-          <h2 className='popup-title'>{project.title}</h2>
-          <p>{project.descriptionShort[language]}</p>
-        </header>
-        <section className='project-body'>
-        <div className='project-content'>
-          <div>
-            <h3>{language === 'fr' ? 'Fonctionnalités' : 'Features'}</h3>
-            <ul className='features-list'>
-              {project.descriptionLong[language].map(item => <li key={item.id}>{item.text}</li>)}
-            </ul>
-          </div>
+      <section className="popup-background">
+        <button
+          className="close-button"
+          type="button"
+          aria-label="fermer"
+          onClick={() => {
+            setIsOpen(false);
+          }}
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <div className="project-container">
+          <header className="popup-header">
+            <h2 className="popup-title">{project.title}</h2>
+            <p>{project.descriptionShort[language]}</p>
+          </header>
+          <section className="project-body">
+            <div className="project-content">
+              <div>
+                <h3>{language === 'fr' ? 'Fonctionnalités' : 'Features'}</h3>
+                <ul className="features-list">
+                {project && project.descriptionLong[language] && project.descriptionLong[language].map((item) => (
+                    <li key={item.id}>{item.text}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="project-links">
+              <a href={project.githubLink} target="_blank" rel="noreferrer">
+                GitHub
+              </a>
+              <a href={project.liveDemo} target="_blank" rel="noreferrer">
+                Live Demo
+              </a>
+            </div>
+            <div id="mimic-octopus">
+              <img
+                src={MimicOctopus}
+                alt="Pieuvre mimétique"
+                className="sea-gif"
+              ></img>
+            </div>
+          </section>
         </div>
-        <div className="project-links">
-          <a href={project.githubLink} target="_blank" rel="noreferrer">GitHub</a>
-          <a href={project.liveDemo} target="_blank" rel="noreferrer">Live Demo</a>
-        </div>
-        <div id="mimic-octopus">
-        <img src={MimicOctopus} alt="Pieuvre mimétique" className="sea-gif"></img>
-        </div>
-        </section>
-      </div>
-    </section>
+      </section>
     </div>
-
   );
 };
 
