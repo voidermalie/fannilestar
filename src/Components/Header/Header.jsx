a53ef401d5c8428a7516c8d59202831ceb0f774c
voidermalie <lestarfanny@gmail.com>
2023-08-25 16:22:28 +0200

    profile picture header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,46 @@
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
+import { useState, useRef } from 'react';
 
 import LanguageButton from '../LanguageButton/LanguageButton';
 import seaAngel from '../../assets/sea_angel.gif';
+import fanniAngel from '../../assets/profile_picture.jpg';
 
 const Header = () => {
+  const [showProfilePicture, setShowProfilePicture] = useState(false);
+  const hideProfilePictureTimeout = useRef(null);
+  
+    const handleMouseEnter = () => {
+      setShowProfilePicture(true);
+      clearTimeout(hideProfilePictureTimeout.current);
+    };
+  
+    const handleMouseLeave = () => {
+      hideProfilePictureTimeout.current = setTimeout(() => {
+        setShowProfilePicture(false);
+      }, 1000); // Adjust the delay (in milliseconds) as needed
+    };
 
   return (
     <header id="main-header">
-      <Link to="/" aria-label="Accueil" id="sea-angel-link">
-      <img src={seaAngel} alt="Sea Angel saying hello" className="sea-gif" />
-      </Link>
+      <img
+        src={seaAngel}
+        alt="Sea Angel saying hello"
+        id="sea-angel"
+        className='sea-gif'
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        style={{ opacity: showProfilePicture ? 0 : 1 }}
+      />
+      {showProfilePicture && (
+        <img
+          src={fanniAngel}
+          alt="Fanni saying hello"
+          id="fanni-angel"
+          style={{ opacity: showProfilePicture ? 1 : 0 }}
+        />
+      )}
       <nav>
         <LanguageButton />
       </nav>
