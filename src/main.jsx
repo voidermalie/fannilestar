bedd05c6eae658b0d7d4b5b5d9365c995184b683
voidermalie <lestarfanny@gmail.com>
2023-09-22 13:29:35 +0200

    initialized GA

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga';
+import { REACT_GA_TRACKING_ID } from '../env';
+
 import './index.css';
 import AppRouter from './Router/Router.jsx';
 import DarkModeProvider from './contexts/DarkModeContext';
 import ProjectProvider from './contexts/ProjectContext';
 import LanguageProvider from './contexts/LanguageContext';
+
+// Initialize Google Analytics
+ReactGA.initialize(REACT_GA_TRACKING_ID);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
