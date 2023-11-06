8fb9a2779f92581d86f4c6daf8d8a684c8b9c74e
voidermalie <lestarfanny@gmail.com>
2023-11-06 14:28:52 +0100

    skills update, glitter comp refactoring

--- src/Components/Glitters/Glitters.jsx
+++ src/Components/Glitters/Glitters.jsx
@@ -1,67 +1,64 @@
 import './Glitters.css';
 
-import { useContext } from 'react';
-import { LanguageContext } from '../../contexts/LanguageContext';
-
 import HTML5Glitter from '../../assets/glitters/html.gif';
 import CssGlitter from '../../assets/glitters/css.gif';
 import JavascriptGlitter from '../../assets/glitters/javascript.gif';
 import ReactGlitter from '../../assets/glitters/react.gif';
 import GitGlitter from '../../assets/glitters/git.gif';
-
+import ReactNativeGlitter from '../../assets/glitters/ReactNative.gif';
 import TypescriptGlitter from '../../assets/glitters/typescript.gif';
-
-
 import NodejsGlitter from '../../assets/glitters/nodejs.gif';
 import MongoDBGlitter from '../../assets/glitters/mongodb.gif';
 import NextjsGlitter from '../../assets/glitters/nextjs.gif';
 import FigmaGlitter from '../../assets/glitters/figma.gif';
-import SEOGlitter from '../../assets/glitters/SEO.gif';
-import AccessibilityGlitter from '../../assets/glitters/accessibility.gif';
-import AccessibiliteGlitter from '../../assets/glitters/accessibilite.gif';
+import TailWindGlitter from '../../assets/glitters/tailwind.gif';
 
-import AnglaisGlitter from '../../assets/glitters/anglais.gif';
-import EnglishGlitter from '../../assets/glitters/English.gif';
-import FrancaisGlitter from '../../assets/glitters/francais.gif';
-import FrenchGlitter from '../../assets/glitters/French.gif';
-import HongroisGlitter from '../../assets/glitters/hongrois.gif';
-import HungarianGlitter from '../../assets/glitters/Hungarian.gif';
-import EspagnolGlitter from '../../assets/glitters/espagnol.gif';
-import SpanishGlitter from '../../assets/glitters/Spanish.gif';
+const glittersGroup1 = [
+  { alt: 'HTML5', src: HTML5Glitter },
+  { alt: 'CSS3', src: CssGlitter },
+  { alt: 'JavaScript', src: JavascriptGlitter },
+  { alt: 'TypeScript', src: TypescriptGlitter },
+];
 
+const glittersGroup2 = [
+  { alt: 'React', src: ReactGlitter },
+  { alt: 'Next.js', src: NextjsGlitter },
+  { alt: 'React Native', src: ReactNativeGlitter },
+];
 
+const glittersGroup3 = [
+  { alt: 'Git', src: GitGlitter },
+  { alt: 'Figma', src: FigmaGlitter },
+  { alt: 'TailWind', src: TailWindGlitter }
+];
+
+const glittersGroup4 = [
+  { alt: 'Node.js', src: NodejsGlitter },
+  { alt: 'Mongo DB', src: MongoDBGlitter },
+];
 
 const Glitters = () => {
-  const { language } = useContext(LanguageContext);
-
   return (
     <div className="glitters-container">
       <div className="glitters-group1">
-        <img className="glitter" alt="HTML5" src={HTML5Glitter} />
-        <img className="glitter" alt="CSS3" src={CssGlitter} />
-        <img className="glitter" alt="JavaScript" src={JavascriptGlitter} />
-        <img className="glitter" alt="TypeScript" src={TypescriptGlitter} />
-        <img className="glitter" alt="React" src={ReactGlitter} />
-        <img className="glitter" alt="Next.js" src={NextjsGlitter} />
+        {glittersGroup1.map((item, index) => (
+          <img key={index} className="glitter" alt={item.alt} src={item.src} />
+        ))}
       </div>
       <div className="glitters-group2">
-        <img className="glitter" alt="Git" src={GitGlitter} />
-        <img className="glitter" alt="Figma" src={FigmaGlitter} />
+        {glittersGroup2.map((item, index) => (
+          <img key={index} className="glitter" alt={item.alt} src={item.src} />
+        ))}
       </div>
       <div className="glitters-group1">
-        <img className="glitter" alt="Node.js" src={NodejsGlitter} />
-        <img className="glitter" alt="Mongo DB" src={MongoDBGlitter} />
-      </div>
-      <div className="glitters-group2">
-      <img className="glitter" alt="SEO" src={SEOGlitter} />
-      <img className="glitter" alt="accessibility" src={language === 'fr' ? AccessibiliteGlitter : AccessibilityGlitter}
- />
+        {glittersGroup3.map((item, index) => (
+          <img key={index} className="glitter" alt={item.alt} src={item.src} />
+        ))}
       </div>
       <div className="glitters-group1">
-      <img className="glitter" alt="anglais" src={language === 'fr' ? AnglaisGlitter : EnglishGlitter}/>
-      <img className="glitter" alt="français" src={language === 'fr' ? FrancaisGlitter : FrenchGlitter}/>
-      <img className="glitter" alt="hongrois" src={language === 'fr' ? HongroisGlitter : HungarianGlitter}/>
-      <img className="glitter" alt="espagnol" src={language === 'fr' ? EspagnolGlitter : SpanishGlitter}/>
+        {glittersGroup4.map((item, index) => (
+          <img key={index} className="glitter" alt={item.alt} src={item.src} />
+        ))}
       </div>
     </div>
   );
